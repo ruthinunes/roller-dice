94704aa9fd9e47467a5333584337b9c58c196fc3
ruthinunes <ruthinunesdev@gmail.com>
2023-04-10 17:38:01 -0300

    Uploading javascript file

---
+++ app.js
@@ -0,0 +1,33 @@
+const dicesElement = document.querySelectorAll(".dice");
+const rollButton = document.querySelector("#rollButton");
+const historyListElement = document.querySelector("#historyList");
+
+function rollDice() {
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+  dicesElement.forEach(function (dice) {
+    dice.classList.remove('active');
+    checkHistory(randomNumber, dice);
+  });
+};
+
+function checkHistory(randomNumber, dice) {
+  if (dice.id === `dice-${randomNumber}`) {
+    const dots = Array.from(dice.children);
+    const audio = new Audio('roll.mp3');
+
+    dots.forEach(function (dot) {
+      dot.classList.add('hide');
+
+      setTimeout(function () {
+        dot.classList.remove('hide');
+      }, 1000)
+    });
+    setTimeout(function () {
+      dice.classList.add('active');
+    });
+    audio.play();
+  };
+};
+
+rollButton.addEventListener('click', rollDice);
